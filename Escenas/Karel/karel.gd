79fe58816c86192c0b8a0d455db127f4b7067f71
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-01-22 19:45:26 -0600

    inicialización del repositorio

---
+++ Escenas/Karel/karel.gd
@@ -0,0 +1,8 @@
+extends Node2D
+#Tamaño de celda 
+const TITLE_SIZE : int = 32
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
