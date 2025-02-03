f52f7449af29af74ff4c77aff54b7d4f6bf66f0a
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-02-03 16:51:13 -0600

    continuar con la curva del decay

--- Escenas/Juguete_1/player.gd
+++ Escenas/Juguete_1/player.gd
@@ -4,30 +4,52 @@ enum STATE {FLOOR, FLY, FALLING}
 var maxJumpAir = 1
 var jumpedAir = 0
 
-var direction : Vector2 = Vector2(1.0, 0.0)
+var direction : Vector2 = Vector2.ZERO
+var lastDirection : Vector2 = Vector2.RIGHT
 var gravity = Vector2(0.0, 1.0) * 5.0
-var maxVel = 100.0
+@export var maxVel = 100.0
 
-func _ready() -> void:
-	#motion_mode = MOTION_MODE_FLOATING
-	pass
+@export var start_curve : Curve = null
+@export var end_curve : Curve = null
+
+#Attack decay sustain realeace
+var accel = 0.0
+var curveValue = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	direction = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+	elif Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1 
+	elif Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	
+	if direction.length() <= 0.0:
+		curveValue += delta
+		curveValue = clamp(curveValue, 0.0, 1.0)
+		accel = end_curve.sample(curveValue)
+	else:
+		print(direction)
+		curveValue += delta
+		curveValue = clamp(curveValue, 0.0, 1.0)
+		accel = start_curve.sample(curveValue)
+		lastDirection = direction
+		print(accel)
+	
+
 func _physics_process(delta: float) -> void:
 	
-	velocity = Vector2.ZERO
+	if (is_on_floor() and direction.y > 0) or (is_on_ceiling() and direction.y < 0):
+		lastDirection.y = 0
 	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = 1 + get_wall_adjusment() 
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -1 + get_wall_adjusment()
+	if (direction.x > 0 and get_wall_adjusment() < 0) or (direction.x < 0 and get_wall_adjusment() > 0):
+		lastDirection.x = 0
 	
-	if Input.is_action_pressed("ui_down") and !is_on_floor():
-		velocity.y = 1 
-	elif Input.is_action_pressed("ui_up") and !is_on_ceiling():
-		velocity.y = -1
-	
-	velocity = velocity.normalized() * maxVel * delta
+	velocity = lastDirection.normalized() * (accel*maxVel) * delta
 	move_and_slide()
 	move_and_collide(velocity)
 
