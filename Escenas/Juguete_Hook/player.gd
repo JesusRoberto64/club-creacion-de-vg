b0cbc17e34c11a3e98dec0b0c9abbd2b77c13204
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-03-19 10:32:55 -0600

    hook comentarios

--- Escenas/Juguete_Hook/player.gd
+++ Escenas/Juguete_Hook/player.gd
@@ -34,7 +34,6 @@ func _physics_process(delta: float) -> void:
 	if launching:
 		hook.target_position = ray_lenght
 	
-	
 	# STATE WITH MATCH
 	match cur_state:
 		STATE.WALKING:
@@ -48,8 +47,6 @@ func _physics_process(delta: float) -> void:
 			if Input.is_action_just_pressed("jump") and is_on_floor():
 				velocity.y = -JUMP_VELOCITY
 			
-			# Get the input direction and handle the movement/deceleration.
-			# As good practice, you should replace UI actions with custom gameplay actions.
 			var direction := Input.get_axis("ui_left", "ui_right")
 			if direction:
 				velocity.x = direction * SPEED
@@ -72,9 +69,13 @@ func _physics_process(delta: float) -> void:
 			if Input.is_action_just_pressed("jump"):
 				set_collision_mask_value(1, true)
 				cur_state = STATE.WALKING
+				
+				# Direccion del impulso
 				var impulse_dir = hook.target_position.normalized()
 				velocity.y = -impulse_dir.y * JUMP_VELOCITY
 				impulse = -impulse_dir.x * JUMP_VELOCITY
+				
+				#Resetear variables
 				hook.target_position = Vector2.ZERO
 				ray_lenght = Vector2.ZERO
 				launching = false
@@ -109,7 +110,7 @@ func _on_hook_hooked(_anchor_pos: Vector2) -> void:
 	set_collision_mask_value(1, false)
 	velocity = Vector2.ZERO
 	
-	# WORKS only a -45 Degress angle
+	# Solo sirve para 45° o -45°, se necesitan mapear para otros grados.
 	if cur_direction > 0.0:
 		radian = hook.target_position.angle() 
 	else:
